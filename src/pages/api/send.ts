05c1c8c0be8595d72f7921c13d5fde2e6dfa9de3
akumar <akumar.scripts@gmail.com>
2023-12-31 19:01:10 +0530

    code

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,21 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import EmailTemplate from '@/components/emailTemplate';
-import { Resend } from 'resend';
+import * as nodemailer from "nodemailer"
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log("Senidng email")
-  const { data, error } = await resend.emails.send({
-    from: process.env.SENDER_ADDRESS as string,
-    to: [process.env.SENDER_EMAIL as string],
-    subject: 'Date Fixed',
-    react: EmailTemplate(),
-  });
+    console.log("Senidng email", process.env.SENDER_EMAIL_ADDRESS)
+    const dateTime = req.query.datetime
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.SENDER_EMAIL_ADDRESS,
+        pass: process.env.SENDER_EMAIL_PASSWORD
+        }
+      });
 
-  if (error) {
-    return res.status(400).json(error);
-  }
-
-  res.status(200).json(data);
+    const mailOptions = {
+      from: process.env.SENDER_ADDRESS,
+      to: process.env.RECEIVER_EMAIL,
+      subject: 'Confirmation of Our Upcoming Date',
+      html: `
+        <div>
+          <p style="font-size" 18px;">Congratulations! You have a date on ${dateTime}</p>
+          <img src='https://media1.tenor.com/m/hZw60xfIvkYAAAAC/tkthao219-bubududu.gif' height={280} width={280} />
+        </div>
+      `
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        return res.status(400).json(error);
+      } else {
+        res.status(200).json(info);
+      }
+    });
 };
--- src/pages/yay.tsx
+++ src/pages/yay.tsx
@@ -1,23 +1,52 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { redirect } from 'next/navigation';
+import { FormEventHandler, useEffect, useState } from 'react';
 
 export default function Yay() {
-    // const sendEmail = async () => {
-    //     debugger
-    //     const resp = await fetch("/api/send")
-    //     debugger
-    //     const resp_ = await resp.json()
-    //     debugger
-    // }
+  const [defaultDate, setDefaultDate] = useState("")
+
+  const sendEmail: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault()
+
+    const datetime = e.currentTarget.datetime.value
+    const dt = convertToCustomFormat(datetime)
+    const resp = await fetch(`/api/send?datetime=${dt}`)
+    const resp_ = await resp.json()
+  }
+
+  useEffect(() => {
+    const now = new Date();
+    now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+  
+    now.setMilliseconds(null)
+    now.setSeconds(null)
+  
+    setDefaultDate(now.toISOString().slice(0, -1));
+  }, [])
 
   return (
     <main className='gradient'>
-      <div className='container'>
+      <form className='container' onSubmit={sendEmail}>
         <h2>Yay! Let&apos;s fix a date</h2>
-        {/* <button onClick={sendEmail}>Send Email</button> */}
+        <input type="datetime-local" name="datetime" defaultValue={defaultDate} min={defaultDate} />
+        <button className='button-85'>Book</button>
         <img src='https://media1.tenor.com/m/hZw60xfIvkYAAAAC/tkthao219-bubududu.gif' height={280} width={280} />
-      </div>
+      </form>
     </main>
   )
+}
+
+function convertToCustomFormat(isoString: string) {
+  const options = { 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric', 
+    hour: 'numeric', 
+    minute: 'numeric', 
+    hour12: true 
+  };
+  
+  const date = new Date(isoString);
+  const customFormat = date.toLocaleDateString('en-US', options as any);
+  
+  return customFormat;
 }
